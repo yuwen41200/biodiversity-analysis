d8876080ae6b8e284a9ed5463582a13f3c69bc07
Yun-Chih Chen <yunchih@csie.ntu.edu.tw>
2016-07-07 08:20:48 +0800

    Add Darwin Core Archive processor prototype

--- window.py
+++ window.py
@@ -1,13 +1,17 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+import os
 from OpenGL import GL
-from PyQt5.QtWidgets import QMainWindow
+from PyQt5.QtWidgets import QMainWindow, QFileDialog
 
 class MainWindow(QMainWindow):
 
-    def __init__(self):
+    def __init__(self, leafletMap):
         super().__init__()
+
+        self.datasetFileName = ""
+        self.leafletMap = leafletMap
 
         self.setupUI()
 
@@ -15,8 +19,29 @@ class MainWindow(QMainWindow):
     def setupUI(self):
 
         menubar = self.menuBar()
-        fileMenu = menubar.addMenu('&Import Data')
+        fileMenu = menubar.addMenu("&Import Data")
+        action = menu.addAction("Import Data")
+        action.triggered.connect(self.openNewDataset)
 
         self.setGeometry(300, 200, 1000, 700)
-        self.setWindowTitle('Biodiversity Explorer')
+        self.setWindowTitle("Biodiversity Explorer")
         self.show()
+
+    def openNewDataset():
+        fileName = QFileDialog.getOpenFileName(self, "Open a dataset",
+                os.path.expanduser('~'))
+        if self.datasetFileName != fileName:
+           self.datasetFileName = fileName
+           _, extension = os.path.splitext(fileName)
+           if extension == ".zip":
+               try:
+                   darwinCoreArchive = extractDarwinCoreArchive(fileName)
+               except:
+                   # TODO: show a warning dialog
+                   return
+          elif extension == ".csv":
+              #TODO
+              pass
+          # Other supported file type here
+
+
