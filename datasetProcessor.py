1f8d97e00581f74466a9206e7ee9cfef293710ae
Yun-Chih Chen <yunchih@csie.ntu.edu.tw>
2016-07-08 23:25:22 +0800

    Fix csv parser

--- datasetProcessor.py
+++ datasetProcessor.py
@@ -27,14 +27,27 @@ def extractCsv(csvStr, selectedFields=[]):
     delim = ',' if ',' in lines[0] else '\t'
     fields = lines[0].split(delim)
 
-    selectedFields = filter(lambda f: f in fields, selectedFields)
+    if len(lines) <= 1:
+        raise Exception("Empty csv given")
+        return ([],[])
+
+    selectedFields = list(filter(lambda f: f in fields, selectedFields))
     if selectedFields:
+        selectedIndexes = []
+        for field in selectedFields:
+            if field in fields:
+                selectedIndexes.append(fields.index(field))
+            else:
+                raise Exception("field `%s` is in the dataset" % field)
+
         data = []
-        selectedIndexes = map(lambda f: fields.index(f), selectedFields)
         for line in lines[1:]:
-            _line = line.split(delim)
-            data.append([_line[i] for i in selectedIndexes])
+            if line:
+                _line = line.split(delim)
+                data.append([_line[i] for i in selectedIndexes])
     else:
+        # Default: select all fields
+        selectedIndexes = range(len(fields))
         data = [line.split(delim) for line in lines[1:]]
 
-    return (fields, data)
+    return (selectedIndexes, data)
