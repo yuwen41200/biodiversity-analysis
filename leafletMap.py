6322007ba0359adfaec909c4907d158f661fd81e
Yun-Chih Chen <yunchih@csie.ntu.edu.tw>
2016-07-11 15:04:31 +0800

    add refreshMap

--- leafletMap.py
+++ leafletMap.py
@@ -9,17 +9,41 @@ import folium
 # noinspection PyPep8Naming
 class LeafletMap:
 
-    def __init__(self):
+    def __init__(self,
+                 windowSize=[750,500],
+                 tiles="Stamen Terrain"):
+
+        assert(len(windowSize) == 2)
+
         # Instead of writing to file, just write to memory
         def toHTML(self, **kwargs):
             return self.get_root().render(**kwargs)
         folium.element.Element.toHTML = toHTML
 
         self.webView = QWebEngineView()
+        self.windowSize = windowSize
+        self.tiles = tiles
+        self.dataNum = 0
 
-    def refreshMap(self, darwinCoreData):
-        fields, data = extractCsv(darwinCoreData, ["decimalLatitude", "decimalLongitude"])
 
-    def renderView(self):
-        html = self.leafletMap.toHTML()
+    def refreshMap(self, speciesList, centerCoordinate=None):
+        """
+        rerender folium map, given a list of species
+        param: speciesList, dictionary of {specis name -> list of location}
+        param: centerCoordinate, coordinate of central point in map
+        """
+        if not speciesList:
+            return
+
+        if not self.leafletMap
+            self.leafletMap = folium.Map(location=centerCoordinate,
+                                         tiles=self.tiles,
+                                         width=self.windowSize[0],
+                                         height=self.windowSize[1])
+        for specis, places in data:
+            for place in places:
+                self.leafletMap.simple_marker(location=place, popup=specis)
+
+        # render to webview
+        html = leafletMap.toHTML()
         self.webView.setHtml(html)
