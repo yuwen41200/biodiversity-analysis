ca595bb20c5c9e25ec598c9dbd175361b0f5e2b7
Yun-Chih Chen <yunchih@csie.ntu.edu.tw>
2016-07-22 11:53:36 +0800

    Switch to precise distance formula in correlation calculation

--- src/lib/correlation_calculator.py
+++ src/lib/correlation_calculator.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-from math import cos
-
+from math import cos, asin, sqrt
 
 # noinspection PyPep8Naming
 class CorrelationCalculator:
@@ -36,5 +35,10 @@ class CorrelationCalculator:
 
         lat1, lon1 = p1
         lat2, lon2 = p2
-        return cos(lat1 * 0.01745) * cos(lat2 * 0.01745) * (1 - cos((lon2 - lon1) * 0.01745)) \
-            - cos((lat2 - lat1) * 0.01745)
+        P = 0.0174532925199433
+        val = 0.5 - cos((lat2 - lat1) * P)/2 + \
+              cos(lat1 * P) * cos(lat2 * P) * \
+              (1 - cos((lon2 - lon1) * P)) / 2
+
+        # 2 * 6371 km
+        return 12742 * asin(sqrt(val))
