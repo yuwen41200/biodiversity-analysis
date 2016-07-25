e24ec3e9e0093ff38a5e837d2b6fcee92902e4bb
Yu-wen Pwu <yuwen41200@gmail.com>
2016-07-25 19:12:45 +0800

    Brand new species dialog.

--- src/view/add_species_dialog.py
+++ src/view/add_species_dialog.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 from PyQt5 import QtWidgets
+from PyQt5.QtCore import Qt
 
 
 # noinspection PyPep8Naming
@@ -16,25 +17,46 @@ class AddSpeciesDialog(QtWidgets.QDialog):
         """
 
         super().__init__()
+        self.species = species
         self.newSpecies = ""
 
-        self.comboBox = QtWidgets.QComboBox()
-        self.comboBox.addItems(species)
+        self.treeWidget = QtWidgets.QTreeWidget()
+        self.treeWidget.setColumnCount(1)
+        self.treeWidget.header().hide()
+        self.treeWidget.setSizePolicy(QtWidgets.QSizePolicy(
+            QtWidgets.QSizePolicy.Fixed, QtWidgets.QSizePolicy.Fixed
+        ))
+        self.treeWidget.setMinimumSize(600, 800)
+
+        outerItems = {}
+        for s in species:
+            key = s[0].upper()
+            if key in outerItems:
+                outerItem = outerItems[key]
+                innerItem = QtWidgets.QTreeWidgetItem(outerItem)
+                innerItem.setText(0, s)
+            else:
+                outerItem = QtWidgets.QTreeWidgetItem()
+                outerItem.setText(0, key)
+                outerItems[key] = outerItem
+                innerItem = QtWidgets.QTreeWidgetItem(outerItem)
+                innerItem.setText(0, s)
+
+        self.treeWidget.insertTopLevelItems(0, list(outerItems.values()))
+        self.treeWidget.sortItems(0, Qt.AscendingOrder)
 
         label = QtWidgets.QLabel("&Select a species: ")
-        label.setBuddy(self.comboBox)
+        label.setBuddy(self.treeWidget)
 
         pushButton = QtWidgets.QPushButton("&Add")
         pushButton.setDefault(True)
 
+        self.treeWidget.itemDoubleClicked.connect(self.addSpeciesHandler)
         pushButton.clicked.connect(self.addSpeciesHandler)
 
-        topLayout = QtWidgets.QHBoxLayout()
-        topLayout.addWidget(label)
-        topLayout.addWidget(self.comboBox)
-
         mainLayout = QtWidgets.QVBoxLayout()
-        mainLayout.addLayout(topLayout)
+        mainLayout.addWidget(label)
+        mainLayout.addWidget(self.treeWidget)
         mainLayout.addWidget(pushButton)
         mainLayout.setSizeConstraint(QtWidgets.QVBoxLayout.SetFixedSize)
 
@@ -49,5 +71,7 @@ class AddSpeciesDialog(QtWidgets.QDialog):
         :return: None.
         """
 
-        self.newSpecies = self.comboBox.currentText()
-        self.done(0)
+        if (self.treeWidget.selectedItems() and
+                self.treeWidget.selectedItems()[0].text(0) in self.species):
+            self.newSpecies = self.treeWidget.selectedItems()[0].text(0)
+            self.done(0)
