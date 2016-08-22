286acaf72a17dcd59932185cced72897e482f53d
Yu-wen Pwu <yuwen41200@gmail.com>
2016-08-22 15:45:24 +0800

    Testing.

--- src/app.py
+++ src/app.py
@@ -3,6 +3,7 @@
 
 import os
 import sys
+import multiprocessing
 
 from PyQt5.QtWidgets import QApplication
 
@@ -11,6 +12,9 @@ from view.main_window import MainWindow
 from controller.main_action import MainAction
 
 if __name__ == '__main__':
+
+    multiprocessing.freeze_support()
+    multiprocessing.set_start_method("spawn")
 
     # QTBUG-49940 workaround
     os.environ["LIBOVERLAY_SCROLLBAR"] = "0"
