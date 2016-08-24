cd1905c9c16c05125de38a4cb2cf0ee98063177e
Yu-wen Pwu <yuwen41200@gmail.com>
2016-08-24 14:29:59 +0800

    Almost done on rktw packer.

--- utils/roadkill-packer/roadkillTWPacker.py
+++ utils/roadkill-packer/roadkillTWPacker.py
@@ -1,5 +1,36 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-# TODO: Use https://docs.python.org/3/library/string.html#template-strings.
-pass
+from tkinter import Tk, messagebox, filedialog
+from time import strftime
+from os.path import basename
+from string import Template
+from zipfile import ZipFile
+
+Tk().withdraw()
+path = ''
+
+while not path:
+    messagebox.showinfo('', 'Please select the source file.')
+    path = filedialog.askopenfilename()
+
+with open('templates/eml.xml') as file:
+    eml = file.read()
+
+with open('templates/meta.xml') as file:
+    meta = file.read()
+
+data = {
+    'eml_publication_date': strftime('%Y-%m-%d'),
+    'meta_source_location': basename(path)
+}
+
+eml = Template(eml).substitute(data)
+meta = Template(meta).substitute(data)
+
+with ZipFile('dwca-roadkill-taiwan.zip', 'w') as zip_file:
+    zip_file.writestr('eml.xml', eml)
+    zip_file.writestr('meta.xml', meta)
+    zip_file.write(path, basename(path))
+
+messagebox.showinfo('', 'Successfully packed.')
