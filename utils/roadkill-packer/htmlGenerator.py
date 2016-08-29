ed00a73f812cdb9c13a80446bb459de2f47d58c2
Yu-wen Pwu <yuwen41200@gmail.com>
2016-08-29 17:29:38 +0800

    Rktw packer almost done.

--- utils/roadkill-packer/htmlGenerator.py
+++ utils/roadkill-packer/htmlGenerator.py
@@ -12,6 +12,40 @@ html_beginning = '''<!DOCTYPE html>
     <body>
         <table data-toggle="table" data-pagination="true">
             <thead>
+                <tr>
+                    <th data-sortable="true">id</th>
+                    <th data-sortable="true">family</th>
+                    <th data-sortable="true">taxonRemarks</th>
+                    <th data-sortable="true">scientificName</th>
+                    <th data-sortable="true">vernacularName</th>
+                    <th data-sortable="true">previousIdentifications</th>
+                    <th data-sortable="true">individualCount</th>
+                    <th data-sortable="true">occurrenceRemarks</th>
+                    <th data-sortable="true">modified</th>
+                    <th data-sortable="true">eventRemarks</th>
+                    <th data-sortable="true">institutionCode</th>
+                    <th data-sortable="true">eventDate</th>
+                    <th data-sortable="true">recordedBy</th>
+                    <th data-sortable="true">rightsHolder</th>
+                    <th data-sortable="true">municipality</th>
+                    <th data-sortable="true">rights</th>
+                    <th data-sortable="true">decimalLongitude</th>
+                    <th data-sortable="true">decimalLatitude</th>
+                    <th data-sortable="true">fieldNotes</th>
+                    <th data-sortable="true">identificationVerificationStatus</th>
+                    <th data-sortable="true">recordNumber</th>
+                    <th data-sortable="true">materialSampleID</th>
+                    <th data-sortable="true">locationRemarks</th>
+                    <th data-sortable="true">associatedReferences</th>
+                    <th data-sortable="true">associatedMedia</th>
+                    <th data-sortable="true">basisOfRecord</th>
+                    <th data-sortable="true">language</th>
+                    <th data-sortable="true">continent</th>
+                    <th data-sortable="true">country</th>
+                    <th data-sortable="true">countryCode</th>
+                </tr>
+            </thead>
+            <tbody>
 '''
 
 html_ending = '''            </tbody>
@@ -35,21 +69,20 @@ def generate(input_path, output_path):
 
     with open(input_path, newline='') as input_file:
         rows = csv.reader(input_file)
+        next(rows, None)
         with open(output_path, 'w') as output_file:
             output_file.write(html_beginning)
-            is_first_line = True
             for row in rows:
-                if is_first_line:
-                    output_file.write('                <tr>\n')
-                    for column in row:
-                        output_file.write('                    <th data-sortable="true">' + column + '</th>\n')
-                    output_file.write('                </tr>\n')
-                    output_file.write('            </thead>\n')
-                    output_file.write('            <tbody>\n')
-                    is_first_line = False
-                else:
-                    output_file.write('                <tr>\n')
-                    for column in row:
+                output_file.write('                <tr>\n')
+                for no, column in enumerate(row):
+                    if no < 23:
                         output_file.write('                    <td>' + column + '</td>\n')
-                    output_file.write('                </tr>\n')
+                    else:
+                        output_file.write('                    <td><a href="' + column + '" target="_blank">' + column + '</a></td>\n')
+                output_file.write('                    <td>HumanObservation</td>\n')
+                output_file.write('                    <td>zh-Hant-TW</td>\n')
+                output_file.write('                    <td>Asia</td>\n')
+                output_file.write('                    <td>Taiwan</td>\n')
+                output_file.write('                    <td>TW</td>\n')
+                output_file.write('                </tr>\n')
             output_file.write(html_ending)
